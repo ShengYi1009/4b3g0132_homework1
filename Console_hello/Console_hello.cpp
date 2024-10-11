3ee636d16fea91313c2434c95bce800aa01a483e
4b3g0132@office.stust.edu.tw <4b3g0132@office.stust.edu.tw>
2024-10-11 22:44:32 +0800

    四則運算

--- Console_hello/Console_hello.cpp
+++ Console_hello/Console_hello.cpp
@@ -2,19 +2,23 @@
 //
 
 #include <iostream>
+#include"Calculator.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double x, y, result;
+    char oper;
+
+    Calculator c;
+    while (true)
+    {
+        char ch;
+        std::cout << "請輸入運算子: ";
+        std::cin >> x >> oper >> y;
+        result = c.Calculate(x, oper, y);
+        std::cout << "結果: " << result << std::endl;
+        std::cout << "是否繼續(y/n): ";
+        std::cin >> ch;
+        if (ch != 'y') break;
+    }
 }
-
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
-
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
